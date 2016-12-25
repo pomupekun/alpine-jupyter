bb6f8173d3521eb2ffd082ebcc8aba22137ad3c2
pomupekun <pomupekun@gmail.com>
2016-12-25 23:35:09 +0900

    cmd.sh: modified startup script and fixed python kernel crush problem

--- conf/cmd.sh
+++ conf/cmd.sh
@@ -8,6 +8,6 @@ elif [ "$ENV" = "LAB" ]; then
 	echo "***** alpine-jupyter start jupyter-lab mode *****"
 else
 	echo "***** alpine-jupyter start. access localhost:8888 on your brower. *****"
-	exec jupyter notebook --ip=0.0.0.0 --notebook-dir=/jupyter/notebook --config=/jupyter/conf/jupyter_notebook_config.py
+	exec sh -c "jupyter notebook --ip=0.0.0.0 --notebook-dir=/jupyter/notebook --config=/jupyter/conf/jupyter_notebook_config.py"
 fi
 
